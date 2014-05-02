dfb255399d31472fab9b2e0dcacc78e78d3e6aff
Richard <richardwangwang@gmail.com>
2014-05-02 22:39:06 +0800

    Code refine.

--- webmanager/urls.py
+++ webmanager/urls.py
@@ -1,12 +1,15 @@
-#from django.conf.urls import patterns, include, url
-from django.conf.urls.defaults import patterns, include, url
-from django.conf import settings
-import os
+from django.conf.urls import patterns, include, url
+
 
 urlpatterns = patterns('',
-    url(r'^cmd/$', 'webmanage.views.cmd'),
-    url(r'^syncdb/$', 'webmanage.views.index'),
-    url(r'^django_version/$', 'webmanage.views.version'),
-    url(r'^create_admin_user/$', 'webmanage.views.create_admin'),
-    url(r'^test_email/$', 'backends.mail_backend.mail_backend_test'),
+    url(r'^cmd/$', 'webmanager.views.cmd'),
+    url(r'^django_version/$', 'webmanager.views.version'),
+    url(r'^create_admin_user/$', 'webmanager.views.handle_create_admin_req'),
 )
+
+# The following can be used to do mail backend testing
+try:
+    import backends.mail_backend
+    urlpatterns.extend([url(r'^test_email/$', 'backends.mail_backend.mail_backend_test')])
+except ImportError:
+    pass
--- webmanager/views.py
+++ webmanager/views.py
@@ -4,13 +4,19 @@
 from django.http import HttpResponse
 import sys
 import django
-from utils.django_utils import retrieve_param
+from ufs_utils.django_utils import retrieve_param
 from django.core.management import execute_from_command_line
+from django.contrib.auth import models as auth_models
 
 try:
-    import django_commands_dict
-except:
-    pass
+    from keys.admin_pass import default_admin_password, default_admin_user
+except ImportError:
+    from keys_template.admin_pass import default_admin_password, default_admin_user
+
+try:
+    import django_commands_dict.django_commands_dict as django_commands_dict
+except ImportError:
+    django_commands_dict = None
 
 import django.core.management as core_management
 
@@ -18,6 +24,7 @@ import django.core.management as core_management
 def cmd(request):
     data = retrieve_param(request)
     import StringIO
+
     old_out = sys.stdout
     log_out = StringIO.StringIO()
     sys.stdout = log_out
@@ -27,34 +34,15 @@ def cmd(request):
     # manage.py here is not used in execute_from_command_line, it is just used to occupy the position.
     command_line_param = ["manage.py"]
     command_line_param.extend(params)
-    core_management._commands = django_commands_dict.django_commands_dict
+    # The following is for SAE or BAE etc. which does not allow command directory scanning. We must create
+    # the commands dict before upload the codes to server
+    if not (django_commands_dict is None):
+        core_management._commands = django_commands_dict
     execute_from_command_line(command_line_param)
 
     result = log_out.getvalue()
     sys.stdout = old_out
-    return HttpResponse(result.replace("\n","<br/>"))
-
-
-def index(request):
-    import StringIO
-    from django.core.management.commands.syncdb import Command as SyncDb
-    from django.db import DEFAULT_DB_ALIAS
-    saveout = sys.stdout
-    log_out = StringIO.StringIO()  
-    sys.stdout = log_out
-    '''
-    try:
-        from management import execute_from_command_line
-    except:
-        from django.core.management import execute_from_command_line
-
-    execute_from_command_line(["manage.py", "syncdb", "--noinput"])
-    '''
-    #load_initial_data should be False so no directory searching for this command
-    SyncDb().handle_noargs(**{"interactive": False, "verbosity": 1, "database": DEFAULT_DB_ALIAS, 'load_initial_data': False})
-    result = log_out.getvalue()
-    sys.stdout = saveout
-    return HttpResponse(result.replace("\n","<br/>"))
+    return HttpResponse(result.replace("\n", "<br/>"))
 
 
 def version(request):
@@ -62,14 +50,10 @@ def version(request):
     return HttpResponse(django.VERSION)
 
 
-def create_admin(request):
-    #user = User.objects.create_user('richard', 'r@j.cn', 'johnpassword')
-    #from django.contrib.auth.create_superuser import createsuperuser
-    #createsuperuser()
-    from django.contrib.auth import models as auth_models
-    try:
-        from keys.admin_pass import default_admin_password, default_admin_user
-    except:
-         from keys_template.admin_pass import default_admin_password, default_admin_user
+def create_admin():
     auth_models.User.objects.create_superuser(default_admin_user, 'r@j.cn', default_admin_password)
+
+
+def handle_create_admin_req(request):
+    create_admin()
     return HttpResponse('Done<script>window.href="/"</script>')
