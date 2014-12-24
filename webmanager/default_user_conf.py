2700ffef96e33a87c4e31d77788bb49a4184534b
Richard <richardwangwang@gmail.com>
2014-12-24 10:54:00 +0800

    Fixed import issue.

--- webmanager/default_user_conf.py
+++ webmanager/default_user_conf.py
@@ -1,7 +1,7 @@
 try:
-    from keys.template_keys.admin_pass import default_admin_password, default_admin_user
+    from keys.local_keys.admin_pass import default_admin_password, default_admin_user
 except ImportError:
-    from external_git.webmanager.webmanager.management.commands.keys_default.admin_pass import default_admin_password, default_admin_user
+    from management.commands.keys_default.admin_pass import default_admin_password, default_admin_user
     
     
 def get_default_username_and_pass():
