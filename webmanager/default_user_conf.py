97496a5805aa82afff8c3a27283e6f4786043333
Richard <richardwangwang@gmail.com>
2015-03-07 16:05:34 +0800

    Update key getter.

--- webmanager/default_user_conf.py
+++ webmanager/default_user_conf.py
@@ -1,7 +1,7 @@
 try:
     from keys.local_keys.admin_pass import default_admin_password, default_admin_user
 except ImportError:
-    from management.commands.keys_default.admin_pass import default_admin_password, default_admin_user
+    from server_base_packages.webmanager.webmanager.keys_default.admin_pass import default_admin_password, default_admin_user
     
     
 def get_default_username_and_pass():
