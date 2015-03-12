6c53b8a862a585e068895b22e2f0d429ea5358cc
Richard <richardwangwang@gmail.com>
2015-03-12 23:37:51 +0800

    Added oauth admin.

--- webmanager/admin.py
+++ webmanager/admin.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import Permission
 from djangoautoconf.auto_conf_admin_utils import register_all
+from provider.oauth2.models import RefreshToken, AccessToken
 
 __author__ = 'weijia'
 
 register_all([Permission])
+
+
+try:
+    from provider.oauth2.backends import AccessTokenBackend
+    register_all([AccessToken, RefreshToken])
+except ImportError:
+    pass
