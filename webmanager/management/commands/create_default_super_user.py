f5d00ed283da255b8cd2c82b36e19ab9504a7dd4
Richard <richardwangwang@gmail.com>
2016-05-07 11:37:39 +0800

    Check before creating default super user.

--- webmanager/management/commands/create_default_super_user.py
+++ webmanager/management/commands/create_default_super_user.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from djangoautoconf.local_key_manager import get_local_key, ConfigurableAttributeGetter
 
@@ -7,9 +8,11 @@ from web_manage_tools.user_creator import create_admin
 def create_default_admin():
     super_username = get_local_key("admin_account.admin_username", "webmanager.keys_default")
     super_password = get_local_key("admin_account.admin_password", "webmanager.keys_default")
-    create_admin(super_username,
-                 super_password, "r@j.cn")
-    print "default admin created"
+    if not User.objects.filter(username=super_username).exists:
+        create_admin(super_username, super_password, "r@j.cn")
+        print "default admin already created"
+    else:
+        print "default admin created"
 
 
 class Command(BaseCommand):
