3bfef17eb0c4004e3c36747baa6008fc8b10c962
richard <richardwangwang@gmail.com>
2016-05-23 14:46:21 +0800

    Update default_settings.py.

--- webmanager/default_settings.py
+++ webmanager/default_settings.py
@@ -1,16 +1,4 @@
 INSTALLED_APPS += (
-    'bootstrap3',
-    'django_admin_bootstrapped',
-    'django.contrib.admin',
-    'django.contrib.admindocs',
-    # 'south',  # Do not work in SAE
-    # 'mptt',
-    # 'treenav',
-    # 'background_task',
-    # 'django_cron',  # Do not work in SAE
-    'jquery_ui',
-    # 'provider',
-    # 'provider.oauth2',
     'simplemenu',
     'webmanager',
 )
@@ -19,7 +7,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
 )
 
-AUTHENTICATION_BACKENDS += ('django.contrib.auth.backends.ModelBackend', 'guardian.backends.ObjectPermissionBackend')
+AUTHENTICATION_BACKENDS += ('django.contrib.auth.backends.ModelBackend',
+                            'guardian.backends.ObjectPermissionBackend')
 
 ANONYMOUS_USER_ID = -1
 
