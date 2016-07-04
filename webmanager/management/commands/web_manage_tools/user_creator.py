1aa94189f2a8221bf9579cf8917d5151329a49b4
richard <richardwangwang@gmail.com>
2016-07-04 15:08:12 +0800

    Remove commands from web manager.

--- webmanager/management/commands/web_manage_tools/user_creator.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'q19420'
-
-from django.contrib.auth import models
-
-
-def create_admin(username, password, mail):
-    models.User.objects.create_superuser(username, mail, password)
-
