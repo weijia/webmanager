f0886dbc8f690223c1719ddfc775017350affbab
Richard <richardwangwang@gmail.com>
2016-10-23 17:08:15 +0800

    Added basic usernena config.

--- webmanager/default_settings.py
+++ webmanager/default_settings.py
@@ -16,4 +16,13 @@ AUTHENTICATION_BACKENDS += ('django.contrib.auth.backends.ModelBackend',
 
 ANONYMOUS_USER_ID = -1
 
+AUTH_PROFILE_MODULE = 'webmanager.MyProfile'
 
+USERENA_SIGNIN_REDIRECT_URL = '/accounts/%(username)s/'
+LOGIN_URL = '/accounts/signin/'
+LOGOUT_URL = '/accounts/signout/'
+
+#EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+
+USERENA_ACTIVATION_REQUIRED = False
+USERENA_SIGNIN_AFTER_SIGNUP = True
