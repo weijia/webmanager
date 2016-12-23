50bdb59f7629b60d6aa6c9f3f21b447f00476b19
richard <richardwangwang@gmail.com>
2016-12-23 12:40:32 +0800

    Move user login code to djangoautoconf.

--- webmanager/views_oauth2.py
+++ webmanager/views_oauth2.py
@@ -5,6 +5,8 @@ from provider.oauth2.models import AccessToken
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import login
 
+from djangoautoconf.req_with_auth import login_by_django_user
+
 
 def login_from_oauth2(request):
     data = retrieve_param(request)
@@ -15,7 +17,6 @@ def login_from_oauth2(request):
             user_access_token = access_tokens[0]
             user_access_token.expires = timezone.now()
             user_access_token.save()
-            user_instance = user_access_token.user  # User.objects.get(username=user_access_token.user)
-            user_instance.backend = "django.contrib.auth.backends.ModelBackend"
-            login(request, user_instance)
+            django_user_instance = user_access_token.user
+            login_by_django_user(django_user_instance, request)
     return HttpResponseRedirect(target)
